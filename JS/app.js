eb8b9425960b2eda5f6b2957a775fd2f3bfeef92
dinagr <DINAGR@N14655C.netafim.com>
2015-11-18 22:18:03 +0200

    fix errors

--- JS/app.js
+++ JS/app.js
@@ -1,49 +1,9 @@
-/**Data - Resturalnts in Hong Kong**/
-/**Every resturanr has the following data - location (lat,lng), title, description and URL**/
-var initialData = [{
-    lat: 22.277291,
-    lng: 114.169230,
-    title: 'The Butchers Club Burgers',
-    description: 'The Butchers Club launched in April 2013 in Tin Wan,  Aberdeen as a butcher’s shop,  a private dining room and a space for cooking classes. It is the masterpiece of a group of passionate chefs and butchers who together have created a truly unique concept based around high quality dry-aged beef.',
-    URL: 'http://www.butchersclub.com.hk/'
-}, {
-    lat: 22.282780,
-    lng: 114.154383,
-    title: 'Cake-A-Licious',
-    description: 'Cake-A-Licious provides all kinds of beautiful and delicious cakes, made to order and fully tailored to your own designs and specification. Our deliciously moist and artistically designed cakes are suitable for children and adult birthdays, weddings, anniversaries and all other special and memorable occasions.',
-    URL: 'http://www.cakealicious.com.hk/'
-}, {
-    lat: 22.283231,
-    lng: 114.153289,
-    title: 'Saffron bakery',
-    description: 'Saffron Cafe & Bakery, located at 333 East Bay Street, has been serving Charleston locals since 1986. It was opened to provide quality baked goods for its two sister restaurants, but quickly became the favorite bakery for locals.',
-    URL: 'http://www.eatatsaffron.com'
-}, {
-    lat: 22.282445,
-    lng: 114.155447,
-    title: 'Luk Yu Tea House',
-    description: 'This gorgeous teahouse (c 1933), known for its masterful cooking and Eastern art-deco decor, was the haunt of opera artists, writers and painters (including the creator of one exorbitant ink-and-brush gracing a wall), who came to give recitals and discuss the national fate.',
-    URL: 'http://www.lonelyplanet.com/china/hong-kong/restaurants/chinese/luk-yu-tea-house'
-}, {
-    lat: 22.282166,
-    lng: 114.161528,
-    title: 'Maxims Palace',
-    description: 'In 1971, Maxim’s Group introduced a brand new management concept – “Chinese Food, Western Service” – in its first Chinese restaurant, Jade Garden.',
-    URL: 'http://www.maxims.com.hk/'
-}, {
-    lat: 22.282495,
-    lng: 114.153529,
-    title: 'Tai Cheong Bakery‬',
-    description: 'The best egg tart in Hongkong. Crispy on the outside, and fluffy, sweet, and light on the inside. You will want to eat more than one.',
-    URL: 'http://www.taoheung.com.hk/en/brands/tai_cheong_bakery/index_p_2.html'
-}];
-
 var ViewModel = function() {
     var self = this;
     /**the array of visible markers is the one the will be displayed on the mapl and in the list of resturants**/
     self.markersVisible = ko.observableArray([]);
     /**the array of markers will save all the locations - the ones that are displayed and the ones that are not displyed**/
-    self.markers = ko.observableArray([]);
+    self.markersMap = ko.observableArray([]);
     self.infoWindows = ko.observableArray([]);
 
     function initialize() {
@@ -69,16 +29,18 @@ var ViewModel = function() {
                 success: function(data) {
                     /*callback function if succes - Will add the rating received from foursquare to the content of the info window*/
                     item.rating = data.response.groups[0].items[0].venue.rating;
+                    console.log(data.response.photo);
                     if (!item.rating) {
                         item.rating = 'No rating in foursquare';
                     }
-                    marker.content = '<br><div class="labels">' + '<div class="title">' + item.title + '</div><div>' + 'Foursquare rating: ' + item.rating + '</div><p>' + item.description + '</p>' + '<a href=' + item.URL + '>' + item.URL + '</a>' + '</div>';
+                    marker.content = '<br><div class="labels">' + '<div class="title">' + item.title + '</div><div class="rating">Foursquare rating: ' + item.rating + '</div><p>' + item.description + '</p>' + '<a href=' + item.URL + '>' + item.URL + '</a>' + '</div>';
                 },
                 error: function(data) {
                     /*callback function if error - an alert will be activaded to notify the user of the error*/
                     alert("Could not load data from foursquare!");
                 }
             });
+            
             /*creation of new markers*/
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(item.lat, item.lng),
@@ -94,7 +56,7 @@ var ViewModel = function() {
                 }
             });
             self.markersVisible.push(marker);
-            self.markers.push(marker);
+            self.markersMap.push(marker);
             marker.addListener('click', function() {
                 /*if the animation is allready active, clicking again will stop it*/
                 if (marker.getAnimation() == null) {
@@ -109,15 +71,61 @@ var ViewModel = function() {
                 infowindow.open(map, marker);
             });
         });
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        markers = [];
+          // Bias the SearchBox results towards current map's viewport.
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+              return;
+            }
+
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+              marker.setMap(null);
+            });
+            markers = [];
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+              var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+              };
+
+              // Create a marker for each place.
+              markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+              }));
+
+              if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+              } else {
+                bounds.extend(place.geometry.location);
+              }
+            });
+            map.fitBounds(bounds);
+          });
     }
     self.query = ko.observable('');
     self.query.subscribe(function(value) {
         /**mark all markers as invisible and remove them from the visible markers list**/
-        self.markers().forEach(function(item) {
+        self.markersMap().forEach(function(item) {
             item.setVisible(false);
             self.markersVisible.remove(item);
         });
-        self.markers().forEach(function(item) {
+        self.markersMap().forEach(function(item) {
             if (item.title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                 /**if the place is relevant to the search, make the marker visible and add the marker to the visible markers list**/
                 item.setVisible(true);
---
+++ JS/data.js
@@ -0,0 +1,115 @@
+/**data - places and their location**/
+var initialData =[
+        {
+            lat : 22.277291,
+            lng : 114.169230,
+            title : 'The Butchers Club Burgers',
+            description : 'The Butchers Club launched in April 2013 in Tin Wan,  Aberdeen as a butcher’s shop,  a private dining room and a space for cooking classes. It is the masterpiece of a group of passionate chefs and butchers who together have created a truly unique concept based around high quality dry-aged beef.',
+            URL : 'http://www.butchersclub.com.hk/'
+        },
+        {
+            lat :  22.282780,
+            lng : 114.154383,
+            title : 'Cake-A-Licious',
+            description : 'Cake-A-Licious provides all kinds of beautiful and delicious cakes, made to order and fully tailored to your own designs and specification. Our deliciously moist and artistically designed cakes are suitable for children and adult birthdays, weddings, anniversaries and all other special and memorable occasions.',
+            URL : 'http://www.cakealicious.com.hk/'
+        },
+        { 
+            lat :  22.283231,
+            lng : 114.153289,
+            title : 'Saffron bakery',
+            description : 'Saffron Cafe & Bakery, located at 333 East Bay Street, has been serving Charleston locals since 1986. It was opened to provide quality baked goods for its two sister restaurants, but quickly became the favorite bakery for locals.',
+            URL : 'http://www.eatatsaffron.com'
+        },
+        { 
+            lat :  22.282445,
+            lng : 114.155447,
+            title : 'Luk Yu Tea House',
+            description :'This gorgeous teahouse (c 1933), known for its masterful cooking and Eastern art-deco decor, was the haunt of opera artists, writers and painters (including the creator of one exorbitant ink-and-brush gracing a wall), who came to give recitals and discuss the national fate.',
+            URL : 'http://www.lonelyplanet.com/china/hong-kong/restaurants/chinese/luk-yu-tea-house'
+        },
+        {
+            lat :  22.282166,
+            lng : 114.161528,
+            title : 'Maxims Palace',
+            description : 'In 1971, Maxim’s Group introduced a brand new management concept – “Chinese Food, Western Service” – in its first Chinese restaurant, Jade Garden.',
+            URL : 'http://www.maxims.com.hk/'
+        },
+        {
+            lat : 22.275964, 
+            lng : 114.170887,
+            title : 'Yin Yang',
+            description : 'The best egg tart in Hongkong. Crispy on the outside, and fluffy, sweet, and light on the inside. You will want to eat more than one.',
+            URL : 'http://www.yinyang.hk'
+        },
+        {
+            lat : 22.282495, 
+            lng : 114.153529,
+            title : 'Tai Cheong Bakery‬',
+            description : 'Yin Yang is housed in a three-storey 1930s heritage building. Dinner is a tasting menu, but you’ll have to book at least five days in advance. The website mentions a deposit, but that’s negotiable. There are takeaway options as well.',
+            URL : 'http://www.taoheung.com.hk/en/brands/tai_cheong_bakery/index_p_2.html'
+        },
+        {
+            lat : 22.292320, 
+            lng : 114.199405,
+            title : 'Tung Po Seafood Restaurant',
+            description : 'Must-tries here include the crispy, garlicky wind-sand chicken, the sinus-clearing wasabi cucumber salad, the lotus-leaf rice, and the midnight-black squid-ink pasta.',
+            URL : 'http://www.lonelyplanet.com/china/hong-kong/restaurants/other/tung-po-seafood-restaurant'
+        },
+        {
+            lat : 22.238923, 
+            lng : 114.195699,
+            title : 'Verandah',
+            description : 'A meal in the grand Verandah, run by the prestigious Peninsula, is a special occasion indeed.',
+            URL : 'http://www.therepulsebay.com'
+        },
+        {
+            lat : 22.379549, 
+            lng : 114.271707,
+            title : 'Loaf On',
+            description : 'The motto here is: eat what they hunt. This three-storey Michelin-star restaurant is where fish freshly caught from the Sai Kung waters in the morning lands on customers’ plates by midday.',
+            URL : 'http://www.lonelyplanet.com/china/hong-kong/restaurants/chinese/loaf-on'
+        },
+        {
+            lat : 22.286638, 
+            lng : 114.156595,
+            title : 'Lung King Heen',
+            description : 'The world’s first Chinese restaurant to receive three stars from the Michelin people, still retains them.',
+            URL : 'http://www.fourseasons.com/hongkong'
+        },
+        {
+            lat : 22.282429, 
+            lng : 114.155851,
+            title : 'Boss',
+            description : 'Awarded one Michelin star, the Boss is a perfectionist. Flawless service, austere modern decor, and a meticulous kitchen point to high expectations being imposed.',
+            URL : 'http://www.theboss1.com'
+        },
+        {
+            lat : 22.284566, 
+            lng : 114.152637,
+            title : 'Chairman',
+            description : 'Understated faux-retro decor and warm service impart a homely feel at this upmarket place serving Cantonese classics with a healthy twist.',
+            URL : 'http://www.thechairmangroup.com'
+        },
+        {
+            lat : 22.396428, 
+            lng : 114.109497,
+            title : 'NUR',
+            description : 'NUR is known for delicious and healthy food that\'s presented like poetry. Headed by Chef Nurdin Topham, who spent time at Copenhagen\'s Noma, NUR forages local farms for organic produce and grows herbs on its terrace for two highly creative tasting menus',
+            URL : 'http://www.nur.hk/restaurant'
+        },
+        {
+            lat : 22.286638, 
+            lng : 114.156595,
+            title : 'Caprice',
+            description : 'In contrast to its opulent decor, Caprice, with two Michelin stars, has a straightforward menu. The meals are masterfully crafted from ingredients flown in daily from France.',
+            URL : 'http://www.fourseasons.com/hongkong'
+        },
+        {
+            lat : 22.285959, 
+            lng : 114.150468,
+            title : 'Tim’s Kitchen',
+            description : 'This two-floor restaurant is considered one of Hong Kong\'s best – as evidenced by the Michelin honour and the praises lavished by local gourmands.',
+            URL : 'http://www.lonelyplanet.com/china/hong-kong/restaurants/chinese/tims-kitchen'
+        }
+    ];
